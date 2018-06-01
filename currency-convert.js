a81c3875b0a7978d5ae953dd99e4d47f1aad5645
fcarlone <frankcarlone@gmail.com>
2018-06-01 14:04:13 -0400

    Add currency-convert function

--- currency-convert.js
+++ currency-convert.js
@@ -2,16 +2,7 @@
 // load axios module
 const axios = require('axios');
 
-// use standard Promises
-// const getExchangeRate = (from, to) => {
-//   return axios.get('http://data.fixer.io/api/latest?access_key=66a4222cd9f915f9f1a198a96f9ca6d6').then((response) => {
-//     const euro = 1 / response.data.rates[from];
-//     const rate = euro * response.data.rates[to];
-//     return rate;
-//   })
-// };
-
-// use async
+// async
 const getExchangeRate = async (from, to) => {
   const response = await axios.get('http://data.fixer.io/api/latest?access_key=66a4222cd9f915f9f1a198a96f9ca6d6');
   const euro = 1 / response.data.rates[from];
@@ -19,33 +10,36 @@ const getExchangeRate = async (from, to) => {
   return rate;
 };
 
-// use standard Promises
-// const getCountries = (currencyCodes) => {
-//   return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCodes}`).then((response) => {
-//     return response.data.map((country) => {
-//       return country.name;
-//     });
-//   });
-// };
-
-// use async
+// async
 const getCountries = async (currencyCodes) => {
   const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCodes}`);
   return response.data.map((country) => country.name);
-
-  // return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCodes}`).then((response) => {
-  //   return response.data.map((country) => {
-  //     return country.name;
-  //   });
-  // });
 };
 
-// get actual exchange rate
-getExchangeRate('USD', 'CAD').then((rate) => {
-  console.log(rate);
-});
+// convert currency with Promise
+const convertCurrency = (from, to, amount) => {
+  // fix scoping issues
+  let convertedAmount;
+  return getExchangeRate(from, to).then((rate) => {
+    convertedAmount = (amount * rate).toFixed(2);
+    // console.log(convertedAmount);
+    return getCountries(to);
+  }).then((countries) => {
+    // console.log(countries)
+    return `${amount.toFixed(2)} ${from} is worth ${convertedAmount} ${to}.  You can spend it in the following countries: ${countries.join(', ')}`;
+  });
+};
 
-// get list of countries
-getCountries('EUR').then((countries) => {
-  console.log(countries);
-})
+// // get exchange rate
+// getExchangeRate('USD', 'CAD').then((rate) => {
+//   console.log(rate);
+// });
+//
+// // get list of countries
+// getCountries('EUR').then((countries) => {
+//   console.log(countries);
+// });
+
+convertCurrency('CAD', 'USD', 20).then((message) => {
+  console.log(message);
+});
