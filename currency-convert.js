c1acb0532bfc7c3b81efebafefcf5c48222694d4
fcarlone <frankcarlone@gmail.com>
2018-06-01 14:31:13 -0400

    Remove functions using Promises

--- currency-convert.js
+++ currency-convert.js
@@ -2,7 +2,7 @@
 // load axios module
 const axios = require('axios');
 
-// async
+// async function
 const getExchangeRate = async (from, to) => {
   const response = await axios.get('http://data.fixer.io/api/latest?access_key=66a4222cd9f915f9f1a198a96f9ca6d6');
   const euro = 1 / response.data.rates[from];
@@ -10,34 +10,18 @@ const getExchangeRate = async (from, to) => {
   return rate;
 };
 
-// async
+// async function
 const getCountries = async (currencyCodes) => {
   const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCodes}`);
   return response.data.map((country) => country.name);
 };
 
-// convert currency with Promise
-// const convertCurrency = (from, to, amount) => {
-//   // fix scoping issues
-//   let convertedAmount;
-//   return getExchangeRate(from, to).then((rate) => {
-//     convertedAmount = (amount * rate).toFixed(2);
-//     // console.log(convertedAmount);
-//     return getCountries(to);
-//   }).then((countries) => {
-//     // console.log(countries)
-//     return `${amount.toFixed(2)} ${from} is worth ${convertedAmount} ${to}.  You can spend it in the following countries: ${countries.join(', ')}`;
-//   });
-// };
-
 // async function
 const convertCurrency = async (from, to, amount) => {
   const exchangeRate = await getExchangeRate(from, to);
-  // console.log(`exchangeRate: ${exchangeRate}`);
-  const convertedAmount = (amount * exchangeRate).toFixed(2);
-  // console.log(`Converted Amount: ${convertedAmount}`);
   const countriesList = await getCountries(to);
-  // console.log(`Countries List: ${countriesList.join(', ')}`)
+  const convertedAmount = (amount * exchangeRate).toFixed(2);
+  
   return `${amount.toFixed(2)} ${from} is worth ${convertedAmount} ${to}.  You can spend it in the following countries: ${countriesList.join(', ')}`
 };
 
